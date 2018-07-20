240afa25bc545cdc73b39ee42913834934ad6c5b
Danny Lin <danny.l@ljit.io>
2018-07-20 14:30:21 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import {
+	StyleSheet,
+	Text,
+	View,
+} from "react-native";
+import Verification from "react-native-verification";
+
+
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.verificationCode = "";
+	}
+	_handlerGetVerficationCode = (value) => {
+		this.verificationCode = value;
+		console.log("Verification code = " + this.verificationCode);
+	}
+	render() {
+		return (
+			<View style={styles.container}>
+				<Verification
+					type={"number"}
+					getValue={this._handlerGetVerficationCode}
+				/>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
+
+export default App;
